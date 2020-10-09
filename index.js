03e123fde5b7dbc2bc4d9970620def4b5c5c0f15
Ludvik <Ludvikkvalsvik@gmail.com>
2020-10-09 15:28:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 let band = document.querySelector('.measureband');
 
+let frame = 0
+
 window.addEventListener('wheel', (event)=>{
-    //event.deltaY;
-    band.style.cssText = "background-position-x: calc(1 x -180px);"
+   
+    band.style.cssText = "background-position-x: calc("+ Math.floor(frame) +" * -180px);"
+    frame += 0.1;
+    if(frame > 9) frame = 0
 
 })
