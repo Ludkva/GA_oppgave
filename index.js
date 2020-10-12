c44a176c6b097433be9cfc22fa8dd0c9b99be5bd
Ludkva <ludvikkvalsvik@gmail.com>
2020-10-12 22:56:28 +0200

    Changed values

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let frame = 0
 
 window.addEventListener('wheel', (event)=>{
    
-    band.style.cssText = "background-position-x: calc("+ Math.floor(frame) +" * -180px);"
+    band.style.cssText = "background-position-x: calc("+ Math.floor(frame) +" * -730px);"
     frame += 0.1;
     if(frame > 9) frame = 0
 
